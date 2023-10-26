b7804e7d871b99ab9bf902b6bf7af29e1a21d827
Manoli <uyamis.rahou@gmail.com>
2023-10-26 12:53:56 +0200

    first draft of database interactions

--- database.js
+++ database.js
@@ -16,7 +16,7 @@ db.serialize(() => {
             username text NOT NULL,
             donor integer NOT NULL DEFAULT 0,
             maried_to text,
-            time_travel integer NOT NULL DEFAULT 0,
+            time_travel integer NOT NULL DEFAULT 0
         )
     `;
     db.run(query, (err) => {
@@ -30,15 +30,17 @@ db.serialize(() => {
         CREATE TABLE IF NOT EXISTS Reminder(
             discord_id text NOT NULL,
             command_id text NOT NULL,
-            dTime text NOT NULL,
+            dTime integer NOT NULL,
+            time integer NOT NULL DEFAULT 0,
             enabled integer NOT NULL DEFAULT 0,
             channel_id text NOT NULL,
+            scenario_id text NOT NULL,
+            fixed_cd integer NOT NULL DEFAULT 0,
             PRIMARY KEY (discord_id, command_id),
             FOREIGN KEY (discord_id) 
                 REFERENCES Player(discord_id)
                 ON UPDATE CASCADE
                 ON DELETE CASCADE
-
         )
     `;
     db.run(query2, (err) => {
@@ -47,7 +49,146 @@ db.serialize(() => {
             throw err;
         }
     });
+
+    const query3 = `
+        CREATE TABLE IF NOT EXISTS Cooldowns(
+            command_id text PRIMARY KEY NOT NULL,
+            reduction integer NOT NULL DEFAULT 0
+        )
+    `;
+    db.run(query3, (err) => {
+        if (err) {
+            console.log(err);
+            throw err;
+        }
+    });
 });
+
+function getReminders() {
+    return new Promise((resolve, reject) => {
+        const now = Date.now();
+        const subquery = `(SELECT reduction FROM Cooldowns WHERE Cooldowns.command_id = Reminder.command_id)`;
+        const query = `
+            SELECT * FROM Reminder
+            WHERE enabled = 1
+            AND CASE 
+                WHEN fixed_cd = 1 THEN dTime+time <= ${now}
+                ELSE dTime-dTime*${subquery}/100+time <= ${now}
+            END
+        `;
+        db.all(query, [], (err, rows) => {
+            if (err) {
+                reject(err);
+            }
+            resolve(rows);
+        });
+    });
+}
+
+function disableReminder(discord_id, command_id) {
+    return new Promise((resolve, reject) => {
+        const query = `
+            UPDATE Reminder
+            SET enabled = 0
+            WHERE discord_id = ?
+            AND command_id = ?
+        `;
+        db.run(query, [discord_id, command_id], (err) => {
+            if (err) {
+                reject(err);
+            }
+            resolve();
+        });
+    });
+}
+
+function insertOrUpdateCooldown(command_id, reduction) {
+    return new Promise((resolve, reject) => {
+        const query = `
+            INSERT OR REPLACE INTO Cooldowns(command_id, reduction)
+            VALUES(?, ?)
+        `;
+        db.run(query, [command_id, reduction], (err) => {
+            if (err) {
+                reject(err);
+            }
+            resolve();
+        });
+    });
+}
+
+function getCooldown(command_id) {
+    return new Promise((resolve, reject) => {
+        const query = `
+            SELECT reduction FROM Cooldowns
+            WHERE command_id = ?
+        `;
+        db.get(query, [command_id], (err, row) => {
+            if (err) {
+                reject(err);
+            }
+            resolve(row.reduction);
+        });
+    });
+}
+
+function insertReminder(reminder) {
+    return new Promise((resolve, reject) => {
+        const query = `
+            INSERT OR REPLACE INTO Reminder(discord_id, command_id, dTime, time, enabled, channel_id, scenario_id, fixed_cd)
+            VALUES(?, ?, ?, ?, ?, ?, ?, ?)
+        `;
+        db.run(query, [reminder.discord_id, reminder.command_id, reminder.dTime, reminder.time, true, reminder.channel_id, reminder.scenario_id, reminder.fixed_cd], (err) => {
+            if (err) {
+                reject(err);
+            }
+            resolve();
+        });
+    });
+}
+
+function insertPlayer(player) {
+    return new Promise((resolve, reject) => {
+        const query = `
+            INSERT OR REPLACE INTO Player(discord_id, username, donor, maried_to, time_travel)
+            VALUES(?, ?, ?, ?, ?)
+        `;
+        db.run(query, [player.discord_id, player.username, player.donor, player.maried_to, player.time_travel], (err) => {
+            if (err) {
+                reject(err);
+            }
+            resolve();
+        });
+    });
+}
+
+function deletePlayer(discord_id) {
+    return new Promise((resolve, reject) => {
+        const query = `
+            DELETE FROM Player WHERE discord_id = ?
+        `;
+        db.run(query, [discord_id], (err) => {
+            if (err) {
+                reject(err);
+            }
+            resolve();
+        });
+    });
+}
+
+function playerExists(discord_id) {
+    return new Promise((resolve, reject) => {
+        const query = `
+            SELECT EXISTS(SELECT * FROM Player WHERE discord_id = ?) AS exist;
+        `;
+        db.get(query, [discord_id], (err, row) => {
+            if (err) {
+                reject(err);
+            }
+            resolve(row.exist);
+        });
+    });
+}
 
 db.close((err) => {
     if (err) {
