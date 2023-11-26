01b381aa907174342b83d26a993c33714023c8ed
Orahou5 <orahou@outlook.fr>
2023-11-26 16:46:39 +0100

    activated daily and weekly

--- imported.js
+++ imported.js
@@ -3,3 +3,5 @@ import "./commands/work.js";
 import "./commands/duel.js";
 import "./commands/adventure.js";
 import "./commands/farm.js";
+import "./commands/daily.js";
+import "./commands/weekly.js";
